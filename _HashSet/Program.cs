8a251b49a5ffc2ceb179c72049d036908a38dc2a
Claudiano-Flores <claudiano145@gmail.com>
2023-01-09 19:25:02 -0300

    Add project files.

---
+++ _HashSet/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _HashSet {
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> set = new HashSet<string>();   
+            
+            set.Add("TV");
+            set.Add("Notebook");
+            set.Add("Tablet");
+
+            Console.WriteLine(set.Contains("Notebook"));
+        }
+    }
+}
